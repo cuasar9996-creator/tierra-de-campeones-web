ffd92c5fdd74ed99fdae9850e478551926b436cc
cuasar9996-creator <cuasar9996@gmail.com>
2026-02-22 12:34:59 -0300

    Delete app directory

--- app/android/app/src/main/kotlin/com/tierradecampeones/tierra_de_campeones_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tierradecampeones.tierra_de_campeones_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- app/read_analysis.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-try:
-    with open('analysis_output.txt', 'r', encoding='utf-16-le') as f:
-        print(f.read())
-except Exception as e:
-    print(f"Error: {e}")
